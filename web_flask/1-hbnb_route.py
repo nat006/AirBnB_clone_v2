2d5cdafa4f55d56a53cea47838ef343d753ad79b
nat006 <nthatipmoloi006@gmail.com>
2024-03-24 14:23:23 +0000

    1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,7 +5,7 @@ This module starts a Flask web application that listens on 0.0.0.0, port 5000
 
 from flask import Flask
 
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
@@ -14,5 +14,5 @@ def hello_hbnb():
     """
     return 'Hello HBNB!'
 
-if name == 'main':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
